f6ec34bcef142224c20aeb0a7c2d328ff77782db
Alex Ablazevics <ablazevix@gmail.com>
2022-04-03 15:59:33 -0400

    first commit

---
+++ explodeTuts/src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Header from "./components/Header.svelte";
+	import Footer from "./components/Footer.svelte";
+	export let name;
+</script>
+
+<style>
+	main {
+		max-width: 960px;
+		margin: 40px auto;
+	}
+</style>
+
+<Header />
+<main>
+	<p>
+		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur
+		libero explicabo harum reprehenderit nesciunt eum animi excepturi nisi
+		mollitia dolorem quia eos unde ipsum eaque maiores laboriosam ea,
+		voluptates fuga?
+	</p>
+</main>
+<Footer />
---
+++ explodeTuts/src/components/Header.svelte
@@ -0,0 +1,21 @@
+<style>
+    header {
+        background: #f7f7f7;
+        padding: 20px;
+    }
+    h1 {
+        margin: 0;
+        text-align: center;
+    }
+    img {
+        max-width: 180px;
+    }
+</style>
+
+<header>
+    <h1>
+        <img
+            src="https://www.explodestudio.com/wp-content/uploads/thegem-logos/logo_750d03ea529b0aee56e43b41123ff6f0_1x.png"
+            alt="logo" />
+    </h1>
+</header>
